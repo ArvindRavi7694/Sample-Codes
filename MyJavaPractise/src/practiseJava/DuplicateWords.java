557f4d48799e766796bc0596d3b57b40b7260406
Arivnd <arvindravi7694@gmail.com>
2024-09-02 16:45:14 +0530

    duplicate words code

---
+++ MyJavaPractise/src/practiseJava/DuplicateWords.java
@@ -0,0 +1,46 @@
+package practiseJava;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class DuplicateWords {
+
+	public static void findDuplicateWords(String inputString) {
+
+		// split
+		String[] words = inputString.split(" ");
+
+		// create a hashmap
+		Map<String, Integer> wordCount = new HashMap<String, Integer>();
+
+		// to check each word is given array:
+		for (String word : words) {
+
+			// if word is present
+			if (wordCount.containsKey(word)) {
+				wordCount.put(word.toLowerCase(), wordCount.get(word) + 1);
+			} else {
+				wordCount.put(word, 1);
+			}
+		}
+
+		// extracting all the keys of map - wordcount
+		Set<String> wordInString = wordCount.keySet();
+
+		System.out.println("Set Word in String -> " + wordInString);
+
+		// loop via all the words in wordCount
+		for (String word : wordInString) {
+			if (wordCount.get(word) > 1) {
+				System.out.println(word + " : " + wordCount.get(word));
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		findDuplicateWords("java practise is done java coding is done exercise is done");
+	}
+
+}
